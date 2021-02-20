3d844d5023e017e5ba74d6c5235aac38af6ef897
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2021-02-20 10:13:28 +0100

    Show score in canvas

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    private int destroyedEnemies;
+    private int score;
+
+    private const int POINTS_PER_ENEMY = 100;
+
+    [SerializeField]
+    private Text destroyedEnemiesText;
+
+    [SerializeField]
+    private Text scoreText;
+
+    private void Awake()
+    {
+        Singleton();
+    }
+    
+    private void Singleton()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddDestroyedEnemy()
+    {
+        destroyedEnemies++;
+        score += POINTS_PER_ENEMY;
+
+        destroyedEnemiesText.text = destroyedEnemies.ToString();
+        scoreText.text = score.ToString();
+    }
+}
