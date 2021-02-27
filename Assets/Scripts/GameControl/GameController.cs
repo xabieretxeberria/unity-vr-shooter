b1daf1921fa4cdb8071af8891dff7e94b3bbf855
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2021-02-27 10:57:49 +0100

    GameController Singleton & win condition

---
+++ Assets/Scripts/GameControl/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    [SerializeField]
+    private GameObject gameCompleteScreen;
+
+    [SerializeField]
+    private GameObject gameFailedScreen;
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Singleton()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void GameFailed()
+    {
+        Debug.Log("Game Failed");
+    }
+
+    public void GameComplete()
+    {
+        Debug.Log("Game Complete!");
+    }
+}
--- Assets/Scripts/GameControl/TimeManager.cs
+++ Assets/Scripts/GameControl/TimeManager.cs
@@ -3,20 +3,13 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    //public static TimeManager instance;
-
-    public const int SECONDS_TO_SURVIVE = 60;
+    public const int SECONDS_TO_SURVIVE = 10;
 
     private float remainingTimeCounter;
     private bool isActive;
 
     [SerializeField]
     private Text remainingTimeText;
-
-    private void Awake()
-    {
-        //Singleton();
-    }
 
     void Start()
     {
@@ -33,6 +26,8 @@ public class TimeManager : MonoBehaviour
         if (remainingTimeCounter < 0f) {
             remainingTimeCounter = 0f;
             isActive = false;
+
+            GameController.instance.GameComplete();
         }
     }
 
@@ -41,19 +36,8 @@ public class TimeManager : MonoBehaviour
         remainingTimeText.text = FormatRemainingTime();
     }
 
-    private void Singleton()
-    {
-        //if (instance == null) {
-        //    instance = this;
-        //} else if (instance != this) {
-        //    Destroy(gameObject);
-        //}
-
-        //DontDestroyOnLoad(gameObject);
-    }
-
     private string FormatRemainingTime()
     {
-        return remainingTimeCounter.ToString("F0");
+        return remainingTimeCounter.ToString("00.00");
     }
 }
